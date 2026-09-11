003852dd948790d61f5d40a76910bf2d17d6bfda
woolim <woolim04choi@gmail.com>
2026-09-11 19:22:18 +0800

    add sendJoinMessageToAll method in SessionManager

--- chat/Session.java
+++ chat/Session.java
@@ -4,8 +4,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
-
 import static chat.CloseUtil.closeAll;
 import static chat.MyLogger.log;
 
@@ -31,28 +29,12 @@ public class Session implements Runnable{
         try {
             String received = input.readUTF();
             name = received.substring(6);
-            log(name + "님 입장");
-            output.writeUTF(name + "님 입장 완료");
+            log(name + "님 채팅 입장");
+            sessionManager.sendJoinMessageToAll(name);
 
             while (true) {
                 received = input.readUTF();
 
-                if (received.contains("/message")) {
-                    String message = received.substring(9);
-                    sendMessageToAll(message, name);
-
-                } else if (received.contains("/change")) {
-                    name = received.substring(8);
-                    output.writeUTF("사용자명 변경 완료: " + name + "로 변경");
-
-                } else if (received.equals("/users")) {
-                    sendUserList();
-
-                } else {
-                    log(name + "님 측에서 연결 종료 요청");
-                    output.writeUTF(name + "님 연결 종료");
-                    break;
-                }
             }
         } catch (IOException e) {
             log(e);
@@ -64,36 +46,21 @@ public class Session implements Runnable{
 
     public synchronized void close() {
         if (closed) return;
-        try {
-            output.writeUTF(name + "님 서버가 비정상적으로 종류되었습니다. /exit를 입력하여 종료해주시기 바랍니다.");
-        } catch (IOException e) {
-            log(e);
-        }
 
         closeAll(socket, input, output);
         closed = true;
         log(name + "님 연결 종료 " + socket);
     }
 
-    private void sendMessageToAll(String message, String name) {
-        for (Session session : sessionManager.getSessions()) {
-            try {
-                session.output.writeUTF(name + ": " + message);
-            } catch (IOException e) {
-                log(e);
-            }
+    public void sendMessage(String message) {
+        try {
+            output.writeUTF(message);
+        } catch (IOException e) {
+            log(e);
         }
     }
 
-
-    private void sendUserList() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append("사용자 목록: \n");
-        for (Session session : sessionManager.getSessions()) {
-            sb.append(session.name + '\n');
-        }
-        String userList = sb.toString();
-        output.writeUTF(userList);
+    public String getName() {
+        return name;
     }
-
 }
--- chat/SessionManager.java
+++ chat/SessionManager.java
@@ -22,7 +22,17 @@ public class SessionManager {
         sessions.clear();
     }
 
-    public List<Session> getSessions() {
-        return sessions;
+    public synchronized void sendMessageToAll(String message) {
+        for (Session session : sessions) {
+            session.sendMessage(message);
+        }
+    }
+
+    public synchronized void sendJoinMessageToAll(String name) {
+        for (Session session : sessions) {
+            if (session.getName() != null) {
+                session.sendMessage(name + "님이 입장했습니다.");
+            }
+        }
     }
 }
